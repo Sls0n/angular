def9197cc87f1132dabc1a023eee3ad674c7b0f8
Silson sapkota <silson007@gmail.com>
2024-01-25 12:41:17 +0545

    models, events, ngModels

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,12 +4,39 @@ import { WishItem } from 'src/shared/models/wishItem';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-   items: WishItem[] = [
+  items: WishItem[] = [
     new WishItem('Go to the movies'),
+    new WishItem('Get Coffee'),
     new WishItem('Go to the store', true),
-   ];
+  ];
+
+  listFilter = '0'; // like state in React
+
+  newWishText = ''; // like value in React
+
   title = 'angular';
+
+  get visibleItems(): WishItem[] {
+    let value = this.listFilter;
+
+    if (value === '0') {
+      return this.items;
+    } else if (value === '1') {
+      return this.items.filter((item) => !item.isComplete);
+    } else {
+      return this.items.filter((item) => item.isComplete);
+    }
+  }
+
+  addNewWish() {
+    this.items.push(new WishItem(this.newWishText));
+    this.newWishText = '';
+  }
+
+  toggleItem(item: WishItem) {
+    item.isComplete = !item.isComplete;
+  }
 }
