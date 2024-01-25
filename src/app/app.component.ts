6f30013de67e27799146c8a3c02377f3b619e718
Silson sapkota <silson007@gmail.com>
2024-01-25 11:45:43 +0545

    ng-templates & components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { WishItem } from 'src/shared/models/wishItem';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+   items: WishItem[] = [
+    new WishItem('Go to the movies'),
+    new WishItem('Go to the store', true),
+   ];
   title = 'angular';
 }
